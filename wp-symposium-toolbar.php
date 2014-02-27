5e43cc642798a581f0ec8bd487fd5781a9094ab8
AlphaGolf_fr <AlphaGolf_fr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-27 20:53:00 +0000

    Update the trunk with 0.27.1 / build 2701 bis repetita
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium-toolbar/trunk@866414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-symposium-toolbar.php
+++ wp-symposium-toolbar.php
@@ -9,7 +9,7 @@ Donate Link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: wp-symposium, toolbar, admin, bar, navigation, nav-menu, menu, menus, theme, brand, branding, members, membership
 Requires at least: WordPress 3.5
 Tested up to: 3.8.1
-Stable tag: 0.27.1
+Stable tag: 0.27.0
 Version: 0.27.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
